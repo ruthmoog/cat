069dff9eb08c29c151842b224d80d1c398f7b7d3
Ruth Baker <ruthmoog@gmail.com>
2019-06-11 14:59:43 +0100

    Include shotgun for autoload

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require 'sinatra'
 
-
 get '/' do
   "Hello, World!"
 end
 
 get '/secret' do
   "Hi there Monika!"
+end
+
+get '/topsecret' do
+  "Hi there Ruth"
+end
+
+get '/supersecret' do
+  "OMG!"
 end
