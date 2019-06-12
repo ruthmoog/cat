6c26a1fd2e84631672409043df0e040667bb2980
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-12 14:32:33 +0100

    params

--- app.rb
+++ app.rb
@@ -17,8 +17,13 @@ get '/supersecret' do
   "OMG!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @greet_with_name = ["Mittens", "Tiddles", "Viking"].sample
   erb(:index)
 end
 
+get '/named-cat' do
+  p params
+  @greet_with_name = params[:name] + " " + params[:surname]
+  erb(:index)
+end
